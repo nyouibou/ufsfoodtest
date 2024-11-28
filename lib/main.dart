70fb92ba56e4020392acbff5a863e663eaca597b
nyouibou <akshaits4@gmail.com>
2024-11-28 12:39:41 +0530

    main updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:ufsfooddeliverytest/view/homescreen/homescreen.dart';
-
-// void main() {
-//   runApp(MainApp());
-// }
-
-// class MainApp extends StatelessWidget {
-//   MainApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
-//   }
-// }
-
 // ignore_for_file: use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
